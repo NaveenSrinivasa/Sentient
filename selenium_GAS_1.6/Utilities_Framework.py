8ae55eb6d88e3697851cce6f2365ba23f89b2e25
Balachander <balachander.baskar@synchronoss.com>
2017-09-19 22:14:30 +0530

    Pointed few more negative test cases to 'UploadMTFNegativeTestValidation method

--- selenium_GAS_1.6/Utilities_Framework.py
+++ selenium_GAS_1.6/Utilities_Framework.py
@@ -22,8 +22,7 @@ def SpawnBrowser(browser, platform):
         print('desired: %s' % desired)
         driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', desired_capabilities=desired)
         session_id = driver.session_id # To get current Session ID
-        print('session_id : %s' %session_id)
-        #driver.session_id = 'b19d6912-06f1-400f-a583-482678f3001f' # To connect with your desirable session
+        #driver.session_id = '60b560b2-759b-44ec-b9f9-06c00a363374' # To connect with your desirable session
 
     elif browser == 'firefox':
         desired = DesiredCapabilities.FIREFOX.copy()
@@ -300,6 +299,7 @@ def BackupScreenshotsAndTestReport(src):
         printFP('Not found given screenshot path directory to back up screenshots : %s' % src)
 
 def FindAndReplace(directory, find, replace, filePattern):
+    print '.',
     if 'Utilities_Framework.py' in filePattern or 'connections.json' in filePattern or 'configurations.json' in filePattern:
         filepath = os.path.join(directory, filePattern)
         #print(filepath)
@@ -328,6 +328,7 @@ def FindAndReplace(directory, find, replace, filePattern):
                         f.write(s)
 
 def InitialDirectorySetup(src):
+    print 'Setting up input files according to user given inputs '
     if os.path.exists(src):
         dirname = os.path.split(src)[1]
         dir_list = glob.glob(src + '_*')
@@ -353,7 +354,6 @@ def InitialDirectorySetup(src):
         s = s.replace('fp/' + dirname, 'fp/' + newdirname)
         with open(filePath, "w") as f:
             f.write(s)
-
         return filePath
     else:
         print('Not found given input path to duplicate input directory : %s' % src)
@@ -362,7 +362,7 @@ def FilePathAndInputDataSetup(userdefinedvariables, directory):
     if directory == 'none':
         directory = userdefinedvariables['seleniumDir'] + '/' + userdefinedvariables['inputfilesDir'] + '/'
     for key, value in userdefinedvariables.items():
-        if not key == 'guidance' and not key == 'devices' and not key == 'browser_name' and not key == 'platform_name' and not key == 'email_recipients' and not key == '172.20.3.50':
+        if not key == 'guidance' and not key == 'devices' and not key == 'browser_name' and not key == 'platform_name' and not key == 'email_recipients' and not 'internet_explorer_machine' in key:
             FindAndReplace(directory, key, value, "*")
         elif key == 'browser_name':
             for i in range(len(userdefinedvariables['browser_name'])):
@@ -391,6 +391,6 @@ def FilePathAndInputDataSetup(userdefinedvariables, directory):
                 value = tmpvalue
             newvalue = value[:-1]
             FindAndReplace(directory, key, newvalue, "configurations.json")
-        elif key == '172.20.3.50':
+        elif 'internet_explorer_machine' in key:
             directory = userdefinedvariables['seleniumDir']
             FindAndReplace(directory, key, value, "Utilities_Framework.py")
